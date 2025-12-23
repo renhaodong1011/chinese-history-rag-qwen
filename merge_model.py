150f9c875d19d792c9621d76bc4abce747e00be3
renhaodong1011 <renhaodong1011@gmail.com>
2025-12-23 20:25:22 +0800

    对Qwen-2.5-7B-Instruct模型进行了Lora微调，并将RAG系统中的模型替换成了微调后的模型

--- merge_model.py
+++ merge_model.py
@@ -3,3 +3,15 @@ Created on 2025/12/23 by Renhaodong
 Description:
     用于对微调后的模型进行合并。
 """
+from peft import AutoPeftModelForCausalLM
+import torch
+
+OUTPUT_DIR = '/qwen_history_lora/checkpoint-12_23'
+merged_model = AutoPeftModelForCausalLM.from_pretrained(
+    OUTPUT_DIR,
+    device_map="auto",
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True
+)
+merged_model = merged_model.merge_and_unload()  # 合并 LoRA
+merged_model.save_pretrained("./merged_qwen_history")
